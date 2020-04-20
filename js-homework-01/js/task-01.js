03d1e705d811c1138aacbcd1d16a0a13376b9969
Serhii777 <muzykasv72@gmail.com>
2020-04-20 18:37:32 +0300

    Added task-01

---
+++ js-homework-01/js/task-01.js
@@ -0,0 +1,9 @@
+const name = 'Генератор защитного поля';
+let price = 1000;
+console.log(name);
+console.log(price);
+console.log(`Выбран ${name}, цена за штуку ${price} кредитов`);
+price = 2000;
+console.log(price);
+console.log(`Выбран ${name}, цена за штуку ${price} кредитов`);
+
