cc89e7b36e1a4ac9a9cf076663bea7e3f4f7bf79
Serhii777 <muzykasv72@gmail.com>
2020-04-21 09:28:56 +0300

    Added all tasks

--- js-homework-01/js/task-06.js
+++ js-homework-01/js/task-06.js
@@ -2,25 +2,23 @@ let total = 0;
 
 while (true) {
    let input = prompt('Введите число');
-   input = Number(input);
-   console.log(input);
 
    if (input === null) {
       console.log(`Вы ввели cancel. Итерация прекращается.`);
       break;
    }
+
+   input = Number(input);
    const isNotANumber = Number.isNaN(input);
-   console.log(isNotANumber);
 
    if (isNotANumber) {
-      console.log(isNotANumber);
       console.log('Было введено не число, попробуйте еще раз');
+      alert('Было введено не число, попробуйте еще раз');
       continue;
    }
 
    total += input;
    console.log(`Общая сумма чисел равна ${total}`);
 }
-
 console.log(`Cумма чисел равна ${total}`);
 alert(`Общая сумма чисел равна ${total}`);
