befb7afd5a60bb8e59b1fb6f20b7baa5aaf98a4e
Serhii777 <muzykasv72@gmail.com>
2020-04-22 19:06:13 +0300

    Correction tasks

--- js-homework-01/js/task-05.js
+++ js-homework-01/js/task-05.js
@@ -6,32 +6,26 @@ let price = 0;
 switch (inputName) {
    case 'КИТАЙ':
       price = 100;
-      console.log(`Доставка в ${inputName} будет стоить ${price} кредитов`);
-      alert(`Доставка в ${inputName} будет стоить ${price} кредитов`);
       break;
+
    case 'ЧИЛИ':
       price = 250;
-      console.log(`Доставка в ${inputName} будет стоить ${price} кредитов`);
-      alert(`Доставка в ${inputName} будет стоить ${price} кредитов`);
       break;
+
    case 'АВСТРАЛИЯ':
       price = 170;
-      console.log(`Доставка в ${inputName} будет стоить ${price} кредитов`);
-      alert(`Доставка в ${inputName} будет стоить ${price} кредитов`);
       break;
+
    case 'ИНДИЯ':
       price = 80;
-      console.log(`Доставка в ${inputName} будет стоить ${price} кредитов`);
-      alert(`Доставка в ${inputName} будет стоить ${price} кредитов`);
       break;
+      
    case 'ЯМАЙКА':
       price = 120;
-      console.log(`Доставка в ${inputName} будет стоить ${price} кредитов`);
-      alert(`Доставка в ${inputName} будет стоить ${price} кредитов`);
       break;
 
    default:
       price = 0;
       console.log('В вашей стране доставка не доступна');
-      alert('В вашей стране доставка не доступна');
 }
+console.log(`Доставка в ${inputName} будет стоить ${price} кредитов`);
