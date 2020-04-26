7391df3c8226d26be603dcafa84c9209c1e4494d
Serhii777 <muzykasv72@gmail.com>
2020-04-26 15:20:31 +0300

    Added task-01

---
+++ js-homework-02/js/task-01.js
@@ -0,0 +1,15 @@
+const array = [];
+
+const logItems = function (array) {
+   for (let i = 0; i < array.length; i += 1) {
+      const element = `${1 + i} - ${array[i]}`;
+      console.log(element);
+   }
+};
+
+logItems(array);
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
+// logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
