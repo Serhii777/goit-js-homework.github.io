7c6b9f7d2bd37756ac0ff0b98a242eb6b3c574e9
Serhii777 <muzykasv72@gmail.com>
2020-04-28 13:25:25 +0300

    Added tasks-06

--- js-homework-02/js/task-01.js
+++ js-homework-02/js/task-01.js
@@ -11,7 +11,5 @@ logItems(array);
 /*
  * Вызовы функции для проверки работоспособности твоей реализации.
  */
-// logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
+logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
 logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
-
-// TODO: переделать фор
--- js-homework-02/js/task-06.js
+++ js-homework-02/js/task-06.js
@@ -1,29 +1,27 @@
-// let input = '';
-const numbers = [];
-// let number = 0;
+let input = '';
+let numbers = [];
 let total = 0;
 let message = 'Общая сумма чисел равна: ';
 let incorrectInput = 'Было введено не число, попробуйте еще раз';
-let errorMessage = 'Отменено полльзователем';
+let errorMessage = 'Ввод прекращен пользователем';
 
-// while (input !== null) {
-while (true) {
-   let input = prompt('Введите число', 0);
+for (; input !== null; ) {
+   input = prompt('Введите число', 0);
+
+   if (input === null || input === '') break;
    input = Number(input);
-   numbers.push(input);
-   
-   if (input === null) {
-      for (let i = 0; i < numbers.length; i += 1) {
-         total += numbers[i];
-         console.log(number);
-         console.log(total);
-      }
-      console.log(`${message}${total}`);
-      break;
+   const isNotANumber = Number.isNaN(input);
+
+   if (isNotANumber) {
+      alert(incorrectInput);
+      continue;
    }
-   //    }
-   //    console.log(`${message}${total}`);
+   numbers.push(input);
+   console.log(numbers);
+}
+
+for (let number of numbers) {
+   total += number;
 }
 console.log(errorMessage);
-
-// console.log(sumNumbers(input));
+console.log(`${message}${total}`);
