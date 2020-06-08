cfb960c93ff1ba2c7746fac121e91417460d0110
Serhii777 <muzykasv72@gmail.com>
2020-06-09 02:22:49 +0300

    All scripts open

--- js-homework-07/js/task-06.js
+++ js-homework-07/js/task-06.js
@@ -2,7 +2,7 @@ const inputRef = document.getElementById('validation-input');
 let inputLimit = Number(inputRef.dataset.length);
 // const limit = Number(inputRef.getAttribute('data-length'));
 const typeinputLimit = typeof inputLimit;
-console.log(typeinputLimit);
+// console.log(typeinputLimit);
 
 inputRef.addEventListener('blur', event => {
    if (inputRef.value.length === inputLimit) {
