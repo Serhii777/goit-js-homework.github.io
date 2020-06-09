9fc85e7c866ff9eda44108e4eee87a11a0e744c5
Serhii777 <muzykasv72@gmail.com>
2020-06-09 15:37:54 +0300

    Change tasks-06

--- js-homework-07/js/task-06.js
+++ js-homework-07/js/task-06.js
@@ -2,7 +2,6 @@ const inputRef = document.getElementById('validation-input');
 let inputLimit = Number(inputRef.dataset.length);
 // const limit = Number(inputRef.getAttribute('data-length'));
 const typeinputLimit = typeof inputLimit;
-// console.log(typeinputLimit);
 
 inputRef.addEventListener('blur', event => {
    if (inputRef.value.length === inputLimit) {
